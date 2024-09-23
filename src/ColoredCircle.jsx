b5dd4221debb792ea39cd3ec69e175991ad56644
Ali Mousazada <mousazada.ali@gmail.com>
2024-09-23 13:52:46 -0400

    create random circle

--- src/ColoredCircle.jsx
+++ src/ColoredCircle.jsx
@@ -3,24 +3,55 @@ import Circle from "./Circle";
 import ColorButtons from "./ColorButton";
 
 const ColoredCircles = () => {
-  const [circles, setCircles] = useState([
-    "cornflowerblue",
-    "peachpuff",
-    "lavender",
-  ]);
-//   console.log(circles);
+  const [circles, setCircles] = useState([]);
+  //   console.log(circles);
+
+  function getRandom(min = 0, max = 100) {
+    return Math.random() * (max - min) + min;
+  }
 
   const addCircle = (color) => {
-    setCircles((circles) => [...circles, color]);
+    setCircles((circles) => [
+      ...circles,
+      { color, x: getRandom(), y: getRandom() },
+    ]);
+  };
+
+  // const changePosition = idx => {
+  //   setCircles(circles => {
+  //     const copy = [...circles];
+  //     copy[idx].x = getRandom();
+  //     copy[idx].y = getRandom();
+  //     return copy
+  //   })
+  // }
+
+  const changePosition = (idx) => {
+    console.log(idx);
+    setCircles((circles) =>
+      circles.map((cir, i) =>
+        i === idx ? { ...cir, x: getRandom(), y: getRandom() } : cir
+      )
+    );
   };
 
   return (
     <div>
-        <ColorButtons addCircle = {addCircle} option = {['peachpuff' , 'lightsteelblue','paleturquoise']} />
-        <ColorButtons addCircle = {addCircle} option = {['orange' , 'magenta','teal']} />
-        
-      {circles.map((color, idx) => (
-        <Circle color={color} idx={idx} key={idx} />
+      <ColorButtons
+        addCircle={addCircle}
+        option={["peachpuff", "lightsteelblue", "paleturquoise"]}
+      />
+      {/* <ColorButtons addCircle = {addCircle} option = {['orange' , 'magenta','teal']} /> */}
+
+      {circles.map(({ color, x, y }, idx) => (
+        <Circle
+          changePosition={changePosition}
+          color={color}
+          idx={idx}
+          key={idx}
+          x={x}
+          y={y}
+        />
       ))}
     </div>
   );
