e26ff1074f449a8c2cf92ff34168cdf087082e7a
Ali Mousazada <mousazada.ali@gmail.com>
2024-09-22 21:58:40 -0400

    another coloredcircle component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Circle from "./Circle";
+import ColoredCircles from "./ColoredCircle";
 
 function App() {
   return (
     <div>
-      <Circle color="peachpuff" idx={8} />
+      {/* <Circle color="peachpuff" idx={8} />
       <Circle color="lavender" idx={4} />
-      <Circle color="thistle" idx={3} />
+      <Circle color="thistle" idx={3} /> */}
+      <ColoredCircles />
     </div>
   );
 }
