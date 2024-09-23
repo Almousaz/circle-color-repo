7625b6473be388fbd6687e45f6bef585f625834e
Ali Mousazada <mousazada.ali@gmail.com>
2024-09-22 21:14:14 -0400

    create circle component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Circle from "./Circle";
+
+function App() {
+  return (
+    <div>
+      <Circle color="peachpuff" idx={8} />
+      <Circle color="lavender" idx={4} />
+      <Circle color="thistle" idx={3} />
+    </div>
+  );
+}
+
+export default App;
